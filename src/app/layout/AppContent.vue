843b18b1bbb8d850ebdfde73e0b3447e4b735d98
Nils Smolinski <nils.smolinski@lyth.de>
2021-03-28 17:34:26 +0200

    no dashed border

--- src/app/layout/AppContent.vue
+++ src/app/layout/AppContent.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="container shadow-lg mx-auto bg-white pt-2 md:pt-0 mt-28 md:mt-16 h-screen"
-  >
+  <div class="container shadow-lg mx-auto bg-white pt-2 md:pt-0 mt-28 md:mt-16">
     <header class="bg-white shadow">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
@@ -11,7 +9,7 @@
       <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <!-- Replace with your content -->
         <div class="px-4 py-6 sm:px-0">
-          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
+          <div class="border-4 border-gray-200 rounded-lg h-96">
             <slot></slot>
           </div>
         </div>
